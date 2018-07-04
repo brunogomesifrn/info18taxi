3027078709c4f67d1bef4971531b9e9b79d50188
Ivanaldotj <ivanaldotertulianojunior@gmail.com>
2018-07-04 10:24:07 -0300

    inicio do  php da tela de cadastro

---
+++ SiteTaxi/cadastro.php
@@ -0,0 +1,16 @@
+<?php
+
+$vnome=$_GET["nome"];
+$vsobrenome=$_GET["sobrenome"];
+$vnascimento=$_GET["nascimento"];
+$vcpf=$_GET["cpf"];
+$vrua=$_GET["rua"];
+$vnumero=$_GET["numero"];
+$vbairro=$_GET["bairro"];
+$vestado=$_GET["estado"];
+$vcidade=$_GET["cidade"];
+$vcep=$_GET["cep"];
+$vemail=$_GET["email"];
+$vlogin=$_GET["login"];
+$vpass=$_GET["pass"];
+$vpassconfirm=$_GET["passconfirm"];
--- SiteTaxi/login.php
+++ SiteTaxi/login.php
@@ -4,10 +4,14 @@
 	<title>login</title>
 </head>
 <body>
+<?php 
+if(isset($_POST["username"]) || isset($_POST["pass"])) {
+echo "<p>Alguma informação está em faltando.</p>";
+}else{
+$email = $_POST['username'];
+$senha = $_POST['pass'];
+}
 
-<?php
-echo $_POST['username'];
-echo $_POST['pass'];
 ?>
 </body>
 </html>
