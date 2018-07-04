670fe5f672b433cef7796cb6b5035c750c9cd1ef
julianoc18 <giulianocamilotrajano@gmail.com>
2018-07-04 10:57:20 -0300

    Alteração no login

--- SiteTaxi/login.php
+++ SiteTaxi/login.php
@@ -5,15 +5,11 @@
 </head>
 <body>
 <?php 
-if(isset($_POST["username"]) || isset($_POST["pass"])) {
-echo "<p>Alguma informação está em faltando.</p>";
-}else{
-$email = $_POST['username'];
-$senha = $_POST['pass'];
-}
 
 
-<?php
+
+
+
 $usu = $_POST['username'];
 $pass =  $_POST['pass'];
 
@@ -22,6 +18,6 @@ if (($usu == "admin")&&($pass == "123")) {
 }else{
 	echo "Você errou algum dado!";
 }
-
+?>
 </body>
 </html>
