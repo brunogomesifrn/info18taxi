0e8b85818fa65e60bd51bc8890ecc15d3db85c22
Ivanaldotj <ivanaldotertulianojunior@gmail.com>
2018-07-04 11:02:32 -0300

    continuaçao da tela de cadastro

--- SiteTaxi/cadastro.php
+++ SiteTaxi/cadastro.php
@@ -14,3 +14,13 @@ $vemail=$_POST["email"];
 $vlogin=$_POST["login"];
 $vpass=$_POST["pass"];
 $vpassconfirm=$_POST["passconfirm"];
+
+echo "Nome: ".$vnome."<br/>Sobrenome: ".$vsobrenome."<br/>";
+echo "Nascimento: ".$vnascimento."<br/>cpf: ".$vcpf."<br/>";
+echo "Rua: ".$vrua."<br/>Numero: ".$vnumero."<br/>";
+echo "Bairro: ".$vbairro."<br/>Estado: ".$vestado."<br/>";
+echo "Cidade: ".$vcidade."<br/>CEP: ".$vcep."<br/>";
+echo "Email: ".$vemail."<br/>Login: ".$vlogin."<br/>";
+echo "Pass: ".$vpass."<br/>Comfirmar Senha: ".$vpassconfirm."<br/>";
+
+?>
